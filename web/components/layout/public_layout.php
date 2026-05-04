d6d5b6935c7bcbec27103f95b80cb0b6ae26b5df
Abdulrahman El-Saeed <abdulrahmanalsaeed5@gmail.com>
2026-05-04 07:23:10 +0400

    Phase 27: add media tracking script to public layout

--- web/components/layout/public_layout.php
+++ web/components/layout/public_layout.php
@@ -58,6 +58,7 @@ function render_public_layout(string $title, string $description, string $conten
           </div>
         </div>
       </footer>
+      <script src="/assets/js/media-tracking.js" defer></script>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     </body>
     </html>
